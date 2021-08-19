3281a96e3bfd2c208713852971bf093e008070be
Kristina <kristina.voloshina@gmail.com>
2021-08-19 17:08:49 +0300

    [BMA-1249] add test file
    https://dreams.atlassian.net/browse/BMA-1249

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# This is a sample Python script.
+## here some code
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-#  add some text
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('Bookmate team')
